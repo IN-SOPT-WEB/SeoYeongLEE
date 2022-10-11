7edf0da1498f58e9472c0c1a3ba4e17bdc7a9f61
leeseooo <leeseoooo@naver.com>
2022-10-11 13:43:06 +0900

    feat: DropDown 구현

---
+++ week1/velog-clone/index.js
@@ -0,0 +1,20 @@
+const $ = (selector) => document.querySelector(selector);
+
+const dropDown = $('.nav__week');
+const dropBox = $('.dropBox');
+
+function toggleDropBox(){
+    dropBox.style.display = dropBox.style.display === 'none' ? 'block' : 'none';
+}
+
+function changeDropDownText(e){
+    const dropBoxOptions = dropBox.querySelectorAll('p');
+    const dropDownText = dropDown.querySelector('.dropDown__option');
+
+    [...dropBoxOptions].map((dropBoxOption)=>dropBoxOption.classList.remove('dropBox--selected'));
+    e.target.classList.add('dropBox--selected');
+    dropDownText.innerText = e.target.innerText;
+}
+
+dropDown.addEventListener('click',toggleDropBox);
+dropBox.addEventListener('click',changeDropDownText);
