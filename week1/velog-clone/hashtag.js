f361a5cbc83d3169081405366c7e1d71f9a78060
leeseooo <leeseoooo@naver.com>
2022-10-11 14:55:41 +0900

    feat: tag delete 구현

--- week1/velog-clone/hashtag.js
+++ week1/velog-clone/hashtag.js
@@ -1,16 +1,17 @@
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 const form = $(".tag__form");
-const tagStorage = JSON.parse(localStorage.getItem('tags'));
+const tagStorage = JSON.parse(localStorage.getItem("tags")) || [];
 
-function initTags(){
+function initTags() {
   const tagList = $(".tag__list");
-  tagStorage.map((tag)=>{
+  tagStorage.map((tag) => {
     const newTag = document.createElement("li");
     newTag.classList.add("tag");
     newTag.innerText = tag;
     tagList.appendChild(newTag);
-  })
+  });
 }
 
 function createTag(e) {
@@ -19,7 +20,7 @@ function createTag(e) {
   const input = form.querySelector("input");
   const tagList = $(".tag__list");
 
-  if (input.value.match(/^\S/g)) {
+  if (input.value.match(/^\S/g) && !tagStorage.includes(input.value)) {
     const newTag = document.createElement("li");
 
     newTag.innerText = input.value;
@@ -27,11 +28,30 @@ function createTag(e) {
     tagList.appendChild(newTag);
 
     tagStorage.push(input.value);
-    localStorage.setItem('tags',JSON.stringify(tagStorage));
-
-    input.value = "";
+    localStorage.setItem("tags", JSON.stringify(tagStorage));
   }
+  input.value = "";
+  updateTags();
+}
+
+function deleteTag(e) {
+  const tagList = $(".tag__list");
+  tagList.removeChild(e.target);
+
+  const index = tagStorage.indexOf(e.target.innerText);
+  tagStorage.splice(index, 1);
+  localStorage.setItem("tags", JSON.stringify(tagStorage));
+}
+
+function updateTags() {
+  const tags = $$(".tag");
+  [...tags].map((tag) => tag.addEventListener("click", deleteTag));
+}
+
+function init() {
+  initTags();
+  updateTags();
 }
 
 form.addEventListener("submit", createTag);
-initTags();
+init();
